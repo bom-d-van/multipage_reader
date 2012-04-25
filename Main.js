3c627d174d2655ff26b8b9535c5c7c21284dcb27
bom.d.van <bom.d.van@gmail.com>
2012-04-25 17:06:31 +0800

    Introduce iframe again & Enable toggle event

--- Main.js
+++ Main.js
@@ -1,4 +1,4 @@
-(function (undefined) {
+$(function (undefined) {
     // Global Setting
     c = console;
     c.l = function () {
@@ -6,8 +6,23 @@
         c.log(foo);
     };
     // Init
-    
-})();
+    // Select || Unselect a page block
+    $("div.page-block").toggle(function(event) {
+        $(this).addClass("selected-page-block");
+        // showPageInIframe();
+    }, function (e) {
+        $(this).removeClass("selected-page-block");
+    });
+    // Open || Close a iframe window
+    $("img.page-thumbnail").toggle(function (event) {
+        $(this).attr("src").replace(/.*url=/, "");
+        c.l("Open a window");
+        $("nav").after("<div class='page-win' id='page1'><iframe src='http://www.baidu.com'></iframe></div>");
+        $("#page1").draggable().resizable();
+    }, function (event) {
+        c.l("Close a window");
+    });
+});
 
 function test () {
     c.l("testing");
